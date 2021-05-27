eb07c7ffb82f57cde0219e724028c8bed808ca32
KVC\komzoltcev <komzoltcev.a@gmail.com>
2021-05-27 17:01:38 +0300

    Добавил точку входа

---
+++ src/main.js
@@ -0,0 +1,30 @@
+class Application {
+  static main() {
+    // const card = (
+    //   `<div class="flip-card">
+    //     <div class="flip-card_inner">
+    //       <div class="flip-card_front"></div>
+    //       <div class="flip-card_back"></div>
+    //     </div>
+    //   </div>`
+    // );
+    //
+    // const area = document.querySelector(`.game-area_canvas`);
+    // for (let i = 0; i < 9; i++) {
+    //   area.insertAdjacentHTML(`beforeend`, card);
+    // }
+    //
+    // area.childNodes.forEach((it) => {
+    //   const inner = it.querySelector(`.flip-card_inner`);
+    //
+    // });
+    //
+    // for (let i = 1; i <= area.childNodes.length; i++) {
+    //   setTimeout(() => {
+    //     area.childNodes[i - 1].querySelector(`.flip-card_inner`).classList.add(`flip-card__flipped`);
+    //   }, i * 100);
+    // }
+  }
+}
+
+Application.main();
