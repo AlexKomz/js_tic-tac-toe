ed390df8cde35bb64169a7bd4b8a0c18d7d69e9f
KVC\komzoltcev <komzoltcev.a@gmail.com>
2021-05-28 14:23:06 +0300

    Обновил точку входа игры

--- src/main.js
+++ src/main.js
@@ -1,29 +1,15 @@
+import Game from "./game.js";
+import Model from "./model.js";
+import View from "./view.js";
+
+
 class Application {
   static main() {
-    // const card = (
-    //   `<div class="flip-card">
-    //     <div class="flip-card_inner">
-    //       <div class="flip-card_front"></div>
-    //       <div class="flip-card_back"></div>
-    //     </div>
-    //   </div>`
-    // );
-    //
-    // const area = document.querySelector(`.game-area_canvas`);
-    // for (let i = 0; i < 9; i++) {
-    //   area.insertAdjacentHTML(`beforeend`, card);
-    // }
-    //
-    // area.childNodes.forEach((it) => {
-    //   const inner = it.querySelector(`.flip-card_inner`);
-    //
-    // });
-    //
-    // for (let i = 1; i <= area.childNodes.length; i++) {
-    //   setTimeout(() => {
-    //     area.childNodes[i - 1].querySelector(`.flip-card_inner`).classList.add(`flip-card__flipped`);
-    //   }, i * 100);
-    // }
+    const model = new Model();
+    const view = new View(document.querySelector(`.game-area`));
+
+    const game = new Game(view, model);
+    game.init();
   }
 }
 
